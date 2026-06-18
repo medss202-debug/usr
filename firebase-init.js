c0d96dd4a71032161f4c2c9b7d007728f4a39f0c
medss202-debug <medss202@gmail.com>
2026-06-18 23:43:39 +0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-// قمنا هنا باستدعاء جميع أدوات قاعدة البيانات (Firestore) اللازمة للعمل
 import { 
     getFirestore, 
     collection, 
@@ -14,14 +13,14 @@ import {
     serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-// إعدادات مشروعك كما هي لم تتغير
+// إعدادات قاعدة البيانات الجديدة للعميل الثاني
 const firebaseConfig = {
-    apiKey: "AIzaSyD7cxvC6x1i0t4T45EmPeDuEec2tWWr44E",
-    authDomain: "assmk-1fd46.firebaseapp.com",
-    projectId: "assmk-1fd46",
-    storageBucket: "assmk-1fd46.firebasestorage.app",
-    messagingSenderId: "982227752477",
-    appId: "1:982227752477:web:e7dc8502871df5f19098f4"
+    apiKey: "AIzaSyDH3oeKNrAcYA5VuYFA7YzdFELIu90tLh4",
+    authDomain: "lklk-904dd.firebaseapp.com",
+    projectId: "lklk-904dd",
+    storageBucket: "lklk-904dd.firebasestorage.app",
+    messagingSenderId: "116538688244",
+    appId: "1:116538688244:web:481e3efc8b9fa685ac94a1"
 };
 
 // تهيئة التطبيق
@@ -30,7 +29,7 @@ const app = initializeApp(firebaseConfig);
 // الاتصال بقاعدة البيانات
 const db = getFirestore(app);
 
-// هذه الخطوة مهمة جداً: نجعل أدوات قاعدة البيانات متاحة لملف script.js لكي يستخدمها
+// جعل أدوات قاعدة البيانات متاحة لملفات المتجر والآدمن
 window.db = db;
 window.firestore = {
     collection,
@@ -45,6 +44,6 @@ window.firestore = {
     serverTimestamp
 };
 
-// إطلاق إشارة لباقي ملفات المشروع أن الاتصال بقاعدة البيانات جاهز ويمكنهم البدء
+// إطلاق إشارة لباقي ملفات المشروع أن الاتصال بقاعدة البيانات جاهز
 const event = new Event('firebaseReady');
 window.dispatchEvent(event);
